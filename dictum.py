53549a9f865baa4358c428d1c08773a940306b21
rindybell <alexandria.rindybell@gmail.com>
2017-05-07 15:32:02 +0900

    add dictum

---
+++ dictum.py
@@ -0,0 +1,184 @@
+# coding: utf-8
+
+'''
+Created on 2017/04/15
+
+@author: rindybell
+'''
+
+from collections import Counter
+import gensim
+import numpy as np
+
+
+class Dictum(object):
+    '''
+    classdocs
+    '''
+
+    def __init__(self, use_unknown=False, use_pad_symbol=False):
+        '''
+        Constructor
+        '''
+        self.dictum = {}
+        self.term = Counter()
+        self.idf_m = Counter()
+        self.doc_amount = 0.0
+        self.inv_dictum = {}
+        self.is_locked = False
+        self.unknown_symbol = "<u>"
+        self.pad_symbol = "*"
+
+        if use_pad_symbol:
+            self.push(self.pad_symbol)
+
+        if use_unknown:
+            self.push(self.unknown_symbol)
+
+    def push(self, symbol):
+        if self.is_locked == True:
+            return
+
+        if not symbol in self.dictum:
+            self.dictum[symbol] = len(self.dictum)
+            self.inv_dictum[self.dictum[symbol]] = symbol
+
+        self.term[symbol] += 1
+
+    def push_list(self, symbol_list):
+        if self.is_locked == True:
+            return
+
+        map(self.push, symbol_list)
+
+        for symbol in set(symbol_list):
+            self.idf_m[symbol] += 1
+
+        self.doc_amount += 1
+
+        return
+
+    def locking(self):
+        self.locked = True
+
+    def symbol_to_id(self, symbol):
+        if symbol in self.dictum:
+            return self.dictum[symbol]
+
+        return self.dictum[self.unknown_symbol]
+
+    def symbol_to_vec(self, symbol):
+        return self.id_to_vec(self.symbol_to_id(symbol))
+
+    def id_to_symbol(self, _id):
+        if _id in self.inv_dictum:
+            return self.inv_dictum[_id]
+
+        return "<error>"
+
+    def id_to_vec(self, _id):
+        retvec = np.zeros(self.size())
+        retvec[_id] = 1
+
+        return retvec
+
+    def vec_to_id(self, vec):
+        return np.argmax(vec)
+
+    def vec_to_symbol(self, vec):
+        _id = self.vec_to_id(vec)
+        return self.id_to_symbol(_id)
+
+    def symbol_to_idf(self, symbol):
+        if self.idf_m[symbol] == 0:
+            idf = 0.0
+        else:
+            idf = np.log(self.doc_amount / self.idf_m[symbol])
+
+        return (self.symbol_to_id(symbol), idf)
+
+    def size(self):
+        return len(self.dictum)
+
+    def vec_tf(self, symbol_list, is_binary=False):
+        sigma_m = len(symbol_list) * 1.0
+        ret_vec = np.zeros(self.size())
+
+        for _id in self.map_symbol_to_id(symbol_list):
+            if is_binary:
+                ret_vec[_id] = 1
+            else:
+                ret_vec[_id] += 1
+
+        return ret_vec / sigma_m
+
+    def vec_idf(self, symbol_list):
+        ret_vec = np.zeros(self.size())
+
+        symbol_vocab = set(symbol_list)
+
+        for (_id, idf) in self.map_symbol_to_idf(symbol_list):
+            ret_vec[_id] = idf
+
+        return ret_vec
+
+    def vec_tfidf(self, symbol_list):
+        return self.vec_tf(symbol_list) * self.vec_idf(symbol_list)
+
+    def map_symbol_to_id(self, symbol_list):
+        return map(self.symbol_to_id, symbol_list)
+
+    def map_symbol_to_vec(self, symbol_list):
+        return np.array(map(self.symbol_to_vec, symbol_list))
+
+    def map_id_to_symbol(self, id_list):
+        return map(self.id_to_symbol, id_list)
+
+    def map_id_to_vec(self, id_list):
+        return np.array(map(self.id_to_vec, id_list))
+
+    def map_vec_to_id(self, vec_list):
+        return map(self.vec_to_id, vec_list)
+
+    def map_vec_to_symbol(self, vec_list):
+        return map(self.vec_to_symbol, vec_list)
+
+    def map_symbol_to_idf(self, symbol_list):
+        return map(self.symbol_to_idf, symbol_list)
+
+    def map_symbol_to_vec(self, symbol_list, mode="tfidf", do_unit_vec=False):
+        if mode == "tfidf":
+            result_vec = self.vec_tfidf(symbol_list)
+        elif mode == "tf":
+            result_vec = self.vec_tf(symbol_list)
+        elif mode == "idf":
+            result_vec = self.vec_tfidf(symbol_list)
+        else:
+            sys.stderr.write("Error: Undefined command in symbol_list_to_vec.")
+            exit(-1)
+
+        if do_unit_vec == True:
+            return gensim.matutils.unitvec(result_vec, norm="l2")
+        else:
+            return result_vec
+
+    def size(self):
+        return len(self.dictum)
+
+    def pad(self, sequence, max_len, pad_style="right"):
+        assert len(sequence) <= max_len
+        if pad_style == "right":
+            return sequence + (max_len - len(sequence)) * [self.pad_symbol]
+        elif pad_style == "left":
+            return (max_len - len(sequence)) * [self.pad_symbol] + sequence
+        else:
+            sys.stderr.write("Error: Undefined mode in pad.")
+
+    @staticmethod
+    def preprocess_matirx(M, do_zero_center=True, do_normalize=True):
+        if do_zero_center == True:
+            M -= np.mean(M)
+        if do_normalize == True:
+            M /= np.std(M)
+
+        return M
---
+++ test/dtest_dictum.py
@@ -0,0 +1,72 @@
+# coding:utf-8
+#
+# Author: Masayuki Komai
+#
+
+
+""" imports """
+import sys
+import os
+abspath = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(abspath, "../"))
+import argparse
+from dictum import Dictum
+import numpy as np
+""" variables """
+""" arguments """
+
+
+def def_args():
+    parser = argparse.ArgumentParser(
+        description="dictumのテスト")
+
+    parser.add_argument("-t", "--tsv", type=str,
+                        dest="TSV",
+                        help=u"TSVファイル",
+                        default=None,
+                        required=True)
+
+    args = parser.parse_args()
+    options = args.__dict__
+
+    if None in options.values():
+        parser.print_help()
+        exit(-1)
+
+    return options
+
+""" functions """
+
+""" main """
+
+
+def main(options={}):
+    tsv = open(options["TSV"], "r")
+    tsv_file = map(lambda x: x.strip().split("\t"), tsv.readlines())
+    dictum_vocab = Dictum(use_unknown=True, use_pad_symbol=True)
+    dictum_label = Dictum()
+
+    max_len = max(map(lambda x: len(x[1].split(" ")), tsv_file))
+    sequences = map(lambda x: dictum_vocab.pad(
+        x[1].split(" "), max_len), tsv_file)
+    labels = map(lambda x: x[0], tsv_file)
+    map(dictum_vocab.push_list, sequences)
+    map(dictum_label.push, labels)
+    dictum_vocab.locking()
+    dictum_label.locking()
+
+    print np.array(map(dictum_vocab.map_symbol_to_id, sequences))
+
+    for i in map(dictum_vocab.map_id_to_symbol,
+                 map(dictum_vocab.map_symbol_to_id, sequences)):
+        print " ".join(i)
+
+    print np.array(map(dictum_label.symbol_to_id, labels))
+
+    print np.array(map(lambda x: dictum_vocab.map_symbol_to_vec(x, mode="tf"), sequences))
+    print np.array(map(dictum_vocab.map_symbol_to_vec, sequences))
+
+
+if __name__ == "__main__":
+    options = def_args()
+    main(options)
